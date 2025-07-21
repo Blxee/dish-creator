e66554e31946a99d5ccd658adceb571a36c373ea
Blxee <aminedev57@gmail.com>
2025-07-21 06:05:09 +0100

    Added, darija

---
+++ darija.h
@@ -0,0 +1,40 @@
+// darija.h
+#ifndef DARIJA_H
+#define DARIJA_H
+
+// Keywords substitutes (can't redefine actual keywords)
+#define ila        if            // ila sma3t, dir hadchi
+#define mn_ghiro   else          // la_khra 9albak ghlt
+#define awd_ila    while         // w nti katsna
+#define bayna      return        // bayna liya
+#define rja3       return        // rja3 chi 7aja
+#define balak      break        // rja3 chi 7aja
+#define n9az       continue        // rja3 chi 7aja
+
+// Logic
+#define wla        ||            // wla hna wla lhnaya
+#define ou         &&            // o m3a hadchi
+#define machi      !             // machi hna
+
+// Types
+#define l3adad     int
+#define l7arf      char
+#define klma       char*
+#define walo       void
+#define d3if       float
+#define kbir       double
+
+// Constants
+#define SA7        1             // true
+#define KDB        0             // false
+
+// Functions
+#define ktb        printf        // ktb f l'écran
+#define qra        scanf         // qra mn user
+#define bda_mn_hna      main          // t9der tabda mn hna
+
+// Memory
+#define t9sm       malloc        // t9sm chi memoire
+#define balak     free          // 7ayyedha mn RAM
+
+#endif
